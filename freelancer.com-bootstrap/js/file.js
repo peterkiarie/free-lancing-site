8e09b37385195eda86e95668abbd88318c127e85
peterkiarie <68646137+peterkiarie@users.noreply.github.com>
2021-02-09 14:52:00 +0300

    Add files via upload

---
+++ freelancer.com-bootstrap/js/file.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+
+    // SideNav Button Initialization
+    $(".button-collapse").sideNav();
+    // SideNav Scrollbar Initialization
+    var sideNavScrollbar = document.querySelector('.custom-scrollbar');
+    var ps = new PerfectScrollbar(sideNavScrollbar);
+  })
+  
